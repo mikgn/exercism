395e3b636786b8684c8c629335c9903061fc8acc
michamichael <kogan-misha@rambler.ru>
2019-09-08 21:06:21 -0500

    matrix

---
+++ ruby/matrix/matrix.rb
@@ -0,0 +1,33 @@
+#1
+
+class Matrix
+  attr_reader :rows, :columns
+
+  def initialize(matrix)
+    @rows = matrix.each_line.map { |row| row.split.map(&:to_i) }
+    @columns = rows.transpose
+  end
+end
+
+#2
+
+class Matrix
+  def initialize(matrix)
+    @matrix = matrix
+  end
+
+  def rows
+    @rows ||= @matrix.each_line.map { |row| row.split.map(&:to_i) }
+  end
+
+  def columns
+    @columns ||= rows.transpose
+  end
+end
+
+
+
+
+
+
+
