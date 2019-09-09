146c35a07877907f7e1309dc221bb1d5c3e01fdd
michamichael <kogan-misha@rambler.ru>
2019-09-08 21:05:46 -0500

    resistor-color-duo

---
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,7 @@
+class ResistorColorDuo
+  COLORS = %w(black brown red orange yellow green blue violet grey white)
+
+  def self.value(colors)
+    colors.map { |color| COLORS.index(color) }.join.to_i
+  end
+end
