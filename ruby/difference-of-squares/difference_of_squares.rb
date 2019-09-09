e580a62a7be41740175184a1ca6357b086855026
michamichael <kogan-misha@rambler.ru>
2019-09-08 20:52:51 -0500

    difference-of-squares

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,20 @@
+class Squares
+  def initialize(digit)
+    @arr_of_digits = 1.upto(digit).map { |d| d }
+  end
+
+  def square_of_sum
+    arr_of_digits.map { |d| d }.sum**2
+  end
+
+  def sum_of_squares
+    arr_of_digits.map { |d| d**2 }.sum
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
+
+
+
