5e31ebfabf89959695a4b3ec08c7dbb340415d6f
michamichael <kogan-misha@rambler.ru>
2019-09-08 21:06:55 -0500

    high-scores

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,23 @@
+class HighScores
+  attr_reader :scores
+
+  def scores
+    @scores ||= scores
+  end
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+    scores.last
+  end
+
+  def personal_best
+    scores.max
+  end
+
+  def personal_top_three
+    scores.max(3) 
+  end
+end
