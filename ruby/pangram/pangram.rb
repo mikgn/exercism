bd9d3c500fface291a0f818cf63029c218d6256b
michamichael <kogan-misha@rambler.ru>
2019-09-12 03:57:16 -0500

    [a-z] changing to ''.''

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -2,9 +2,8 @@ class Pangram
   ALPHABET = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
 
   def self.pangram?(sentence)
-    ALPHABET.all? { |letter| sentence.downcase.scan(/[a-z]/).include?(letter) }
+    ALPHABET.all? { |letter| sentence.downcase.scan(/./).include?(letter) }
   end
 end
-
 
 
