6eab77b80ddc7ef7cf943c2b1fd643c6f6044878
michamichael <kogan-misha@rambler.ru>
2019-09-12 03:58:35 -0500

    code improvement

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -18,29 +18,33 @@ class Triangle
     end
   end
 
+  def matching_3_sides?
+    @sides_values.uniq.length == 1
+  end
+
+  def matching_2_or_3_sides?
+    @sides_values.uniq.length == 2 || @sides_values.uniq.length == 1
+  end
+
+  def no_matching_sides
+    matching_2_or_3_sides? == false
+  end
+
   def equilateral?
-    if triangle? 
-      @sides_values[0] == @sides_values[1] &&
-      @sides_values[1] == @sides_values[2] 
+    if triangle?
+      matching_3_sides?
     end
   end
 
   def isosceles?
-    if triangle? 
-      @sides_values[0] == @sides_values[1] && 
-      @sides_values[0] + @sides_values[1] > @sides_values[2] ||
-      @sides_values[0] == @sides_values[2] &&
-      @sides_values[0] + @sides_values[2] > @sides_values[1] ||
-      @sides_values[1] == @sides_values[2] &&
-      @sides_values[1] + @sides_values[2] > @sides_values[0] 
+    if triangle?
+      matching_2_or_3_sides?
     end
   end
 
   def scalene?
-    if triangle?  
-      @sides_values[0] != @sides_values[1] &&
-      @sides_values[0] != @sides_values[2] &&
-      @sides_values[1] != @sides_values[2]
+    if triangle?
+      no_matching_sides
     end
   end
 end
