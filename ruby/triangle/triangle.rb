fdfbe0acaff56a123da9cfba0dd7181bb2c62414
michamichael <kogan-misha@rambler.ru>
2019-09-14 17:37:28 -0500

    move checking to the one method

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -18,33 +18,31 @@ class Triangle
     end
   end
 
-  def matching_3_sides?
-    @sides_values.uniq.length == 1
-  end
-
-  def matching_2_or_3_sides?
-    @sides_values.uniq.length == 2 || @sides_values.uniq.length == 1
-  end
-
-  def no_matching_sides
-    matching_2_or_3_sides? == false
+  def matching_sides
+    if @sides_values.uniq.length == 1
+      3
+    elsif @sides_values.uniq.length == 2
+      2
+    else
+      0
+    end
   end
 
   def equilateral?
     if triangle?
-      matching_3_sides?
+      matching_sides == 3
     end
   end
 
   def isosceles?
     if triangle?
-      matching_2_or_3_sides?
+      matching_sides >= 2
     end
   end
 
   def scalene?
     if triangle?
-      no_matching_sides
+      matching_sides == 0
     end
   end
 end
