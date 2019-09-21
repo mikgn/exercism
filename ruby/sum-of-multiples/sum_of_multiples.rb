7155385d70ab6a9bdc673dd62bce17247b0d7554
michamichael <kogan-misha@rambler.ru>
2019-09-21 18:42:22 -0500

    sum_of_multiples  solution

---
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -0,0 +1,27 @@
+class SumOfMultiples
+  def initialize(*numbers)
+    @numbers = numbers
+  end
+
+  def to(num)
+    arr = []
+
+    1.upto(num - 1) .map do |a|
+     @numbers.map { |number| arr << a if a % number == 0 }
+    end
+
+    answer = arr.uniq.inject(:+)
+    
+    if answer == nil
+      0
+    else
+      answer
+    end
+  end
+end
+
+
+
+
+
+
