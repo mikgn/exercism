ca4f32dc6d40c9f73c96fcb6990e0f9646728cc8
michamichael <kogan-misha@rambler.ru>
2019-09-21 20:30:00 -0500

    code improve

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -2,8 +2,7 @@ class Pangram
   ALPHABET = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
 
   def self.pangram?(sentence)
-    ALPHABET.all? { |letter| sentence.downcase.scan(/\p{L}/).include?(letter) }
+    (ALPHABET - sentence.downcase.split('')).empty?
   end
 end
-
 
