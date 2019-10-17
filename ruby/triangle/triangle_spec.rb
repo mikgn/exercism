8fc97b674c10f09d3eed8372a3edc7179d334edb
michamichael <kogan-misha@rambler.ru>
2019-10-17 02:51:08 -0500

    Added rsepc test to triangle tasks

--- ruby/triangle/triangle_spec.rb
+++ ruby/triangle/triangle_spec.rb
@@ -2,6 +2,11 @@ require 'rspec'
 require_relative 'triangle.rb'
 
 describe Triangle do
+
+  it "Expected 'true', triangle [1, 1, 2] is degenerate." do
+    expect(Triangle.new([1, 1, 2]).degenerate?).to eq true
+  end
+
   it "Expected 'true', triangle [2, 2, 2] is equilateral." do
     expect(Triangle.new([2, 2, 2]).equilateral?).to eq true
   end
@@ -18,4 +23,12 @@ describe Triangle do
     expect(Triangle.new([2, 3, 4]).isosceles?).to eq false
   end
 
-end 
+  it "Expected 'true', triangle [2, 3, 4] is not isosceles." do
+    expect(Triangle.new([2, 3, 4]).isosceles?).to eq false
+  end
+
+  it "Expetced 'false', [0, 1, 2] is not triangle." do
+    expect(Triangle.new([0, 1, 2]).triangle_inequality?).to eq false
+  end
+
+end
