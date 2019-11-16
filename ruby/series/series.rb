e9f4347880666a7d43155f19c8d7b0aad408c43c
michamichael <kogan-misha@rambler.ru>
2019-11-15 20:23:37 -0600

    series.rb refactoring

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,23 +1,16 @@
 class Series
   def initialize string_of_digits
-    @string_of_digits = string_of_digits
+    @string_of_digits = string_of_digits.chars
   end
 
   def slices length_of_serie
-    if @string_of_digits.size < length_of_serie
-      raise ArgumentError
-    else
-      arr_of_digits = @string_of_digits.split('')
-      loops = @string_of_digits.size - length_of_serie + 1
-      answer = []
-      loops.times do
-        answer << arr_of_digits.first(length_of_serie).join
-        arr_of_digits.shift
-      end
-      answer
-    end
+    raise ArgumentError if @string_of_digits.size < length_of_serie
+    arr = []
+    @string_of_digits.each_cons(length_of_serie) { |i| arr << i.join }
+    arr
   end
 end
+
 
 
 
