5813b3f2bddf7d4a2ec99619f2b670026dfff709
Michael Kogan <mike@sliday.com>
2020-02-08 03:28:21 -0600

    code improving

--- ruby/resistor-color-trio/resistor_color_trio.rb
+++ ruby/resistor-color-trio/resistor_color_trio.rb
@@ -6,12 +6,13 @@ class ResistorColorTrio
   COLORS =  %w(black brown red orange yellow green blue violet grey white)
 
   def label
-    raise ArgumentError if (@colors - COLORS).empty? != true
-      zeros = "0" * COLORS.index(@colors.last)
-      @colors.pop
-      digits = @colors.map { |color| COLORS.index(color) }.join
-      n = (digits + zeros).to_i
-    if n % 1000 == 0
+    raise ArgumentError unless (@colors - COLORS).empty?
+
+    zeros = "0" * COLORS.index(@colors.last)
+    digits = @colors.first(2).map { |color| COLORS.index(color) }.join
+    n = (digits + zeros).to_i
+
+    if (n % 1000).zero?
       "Resistor value: #{n/1000} kiloohms"
     else
       "Resistor value: #{n} ohms"
@@ -20,9 +21,15 @@ class ResistorColorTrio
 end
 
 
-
-
-
-
-
-
+if defined?(Minitest)
+  describe 'Custom Tests' do
+    describe 'Given the users list of colors' do
+      it 'must not mangle borrowed list' do
+        my_colors = %w(red yellow green)
+        original_colors = my_colors.dup
+        ResistorColorTrio.new(my_colors).label
+        _(my_colors).must_equal original_colors
+      end
+    end
+  end
+end
