bc18a2a091fe90cbe3f1b22a1163d2d656454317
mikgn <kogan.miha@gmail.com>
2021-05-25 21:43:29 +0300

    refactor

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,30 +1,14 @@
 class Anagram
   def initialize(word)
-    @word = word.downcase
+    @word = word
   end
 
-  def match(*possible_ans)
-    @word_arr = @word.split('')
-    @arr_of_possible_an_spl = possible_ans.map { |possible_an| possible_an.map { |a| a.split('') } }
-
+  def match(possible_ans)
     anagrams = []
-
-    @arr_of_possible_an_spl.each do |possible_an_spl|
-      possible_an_spl.each do |word|
-        anagrams << word if @word_arr.sort == word.each { |l| l.downcase }.sort
-      end
+    possible_ans.each do |ans|
+      skip if @word.downcase == ans.downcase
+      anagrams << ans if @word.downcase.chars.sort == ans.downcase.chars.sort
     end
-
-    anagrams = anagrams.map { |i| i.join }
-    anagrams.delete(@word)
     anagrams
   end
 end
-
-
-
-
-
-
-
-
