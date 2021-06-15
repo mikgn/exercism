005f36cb766884af6e1c5e7b3f0ce413cca8f673
mikgn <kogan.miha@gmail.com>
2021-06-15 20:08:31 +0300

    add new pangram solution

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class Pangram
-  ALPHABET = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
-
   def self.pangram?(sentence)
-    (ALPHABET - sentence.downcase.split('')).empty?
+    sentence.downcase.gsub(/[^[a-z]]/, '').chars.uniq.size == 26
   end
 end
 
+# class Pangram
+#   ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+
+#   def self.pangram?(sentence)
+#     (ALPHABET - sentence.downcase.chars).empty?
+#   end
+# end
